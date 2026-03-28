67c67bcfb6a0c2e7937237598d14259e0bc4e7ba
rosterautomation-source <rosterautomation@gmail.com>
2026-03-28 21:52:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Roster Debug App")
+st.title("Roster Test")
 
-try:
 df = pd.read_excel("latest_roster.xlsx")
-st.write("File Loaded Successfully ✅")
+
+st.write("File loaded successfully")
 st.dataframe(df.head())
-except Exception as e:
-st.error("Error loading file")
-st.write(e)
+
+if st.button("Test Button"):
+st.write("App working correctly")
