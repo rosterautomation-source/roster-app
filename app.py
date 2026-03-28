8cd94486c69b6b41e820e6c57870f7d56bdd1a76
rosterautomation-source <rosterautomation@gmail.com>
2026-03-28 21:56:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Roster Test")
+st.title("Test App")
 
-df = pd.read_excel("latest_roster.xlsx")
-
-st.write("File loaded successfully")
-st.dataframe(df.head())
-
-if st.button("Test Button"):
-st.write("App working correctly")
+st.write("Working")
